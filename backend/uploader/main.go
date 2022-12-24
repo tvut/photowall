bb83f2e76a4a1d3bb94b426eb2a93c14213373e0
Tristan Stevens <tvut@outlook.com>
2022-12-23 17:24:59 -0800

    error codes

--- backend/uploader/main.go
+++ backend/uploader/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -17,45 +18,63 @@ func postUpload(c *gin.Context) {
 
 	file, handler, err := c.Request.FormFile("file")
 
-	// Capture any errors that may arise
+	// Capture any errors that may arise from rest parse
 	if err != nil {
-		print("Error getting the file")
-		print(err)
+		log.Println("Error getting the file")
+		log.Println(err)
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"message": err})
 		return
 	}
 
 	defer file.Close()
 
-	fmt.Printf("Uploaded file name: %v\n", handler.Filename)
-	fmt.Printf("Uploaded file size: %v\n", handler.Size)
-	fmt.Printf("File mime type: %+v\n", handler.Header)
+	// Printing out file information //
+	log.Printf("Uploaded file name: %v\n", handler.Filename)
+	log.Printf("Uploaded file size: %v\n", handler.Size)
+	log.Printf("File mime type: %+v\n", handler.Header)
 
 	// Get the file content type and access the file extension
 	fileType := strings.Split(handler.Header.Get("Content-Type"), "/")[1]
-	fmt.Printf("File type: %v\n", fileType)
+	log.Printf("File type: %v\n", fileType)
+	if fileType != "jpeg" {
+		log.Println("Wrong file type, exiting")
+		c.JSON(http.StatusUnsupportedMediaType, gin.H{"message": "Only accepts jpeg"})
+		return
+	}
 
 	today := time.Now()
 	filePath := fmt.Sprintf("%s/%d/%d/%d", FILE_ROOT, today.Year(), today.Month(), today.Day())
 
-	fmt.Printf("Saving to: %s/%s\n", filePath, handler.Filename)
+	log.Printf("Saving to: %s/%s\n", filePath, handler.Filename)
+	// Done printing file information //
 
+	// Create dir path
 	err = os.MkdirAll(filePath, 0755)
 	if err != nil {
 		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err})
+		return
 	}
 
+	// Read file bytes
 	fileBytes, err := ioutil.ReadAll(file)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"message": err})
+		return
 	}
 
+	// Write file to disk
 	err = ioutil.WriteFile(fmt.Sprintf("%s/%s", filePath, handler.Filename), fileBytes, 0644)
 
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err})
+		return
 	}
 
-	fmt.Printf("Successfully uploaded file")
+	log.Printf("Successfully uploaded file")
+	c.IndentedJSON(http.StatusAccepted, gin.H{"message": "success"})
 }
 
 func main() {
